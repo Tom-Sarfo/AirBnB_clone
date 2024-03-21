34b8eb8497bc4b85fbb10cb6400dd56c998f5425
kabute muthoni <kabutemuthoni@gmail.com>
2024-03-20 22:19:39 -0700

    recreating an instance of the dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -4,12 +4,24 @@ from datetime import datetime
 """A base model class representing common attributes and methods for other classes."""
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new instance of the BaseModel class."""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Return a string representation of the object."""
