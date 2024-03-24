7362a29e3df60a3752432396f77a58bf30c8d5b9
Tom-Sarfo <tomsarfodavis@gmail.com>
2024-03-24 13:49:08 -0700

    Update console and filestorage

--- console.py
+++ console.py
@@ -7,15 +7,13 @@ import re
 import shlex
 from models.base_model import BaseModel
 from models import storage
-from models import User
-"""
+from models import user
 from models.user import User
 from models.state import State
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
-"""
 
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
